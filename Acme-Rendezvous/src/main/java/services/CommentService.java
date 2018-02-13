d7cc977109d3db3b6dc029477310c2ec7cf83a9b
Jorge Puente <Jorge Puente>
2018-02-13 16:31:02 +0100

    Modify CommentService

--- Acme-Rendezvous/src/main/java/services/CommentService.java
+++ Acme-Rendezvous/src/main/java/services/CommentService.java
@@ -1,13 +1,19 @@
 
 package services;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.CommentRepository;
+import domain.Admin;
 import domain.Comment;
+import domain.Rendezvous;
+import domain.User;
 
 @Service
 @Transactional
@@ -19,13 +25,46 @@ public class CommentService {
 	private CommentRepository			commentRepository;
 
 	// Supporting services ----------------------------------------------------
-
-
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private AdminService adminService;
+	@Autowired
+	private RendezvousService rendezvousService;
+	
 	// Simple CRUD methods ----------------------------------------------------
 
-	public Comment create() {
+	public Comment createWithRendezvous(Rendezvous rendezvous) {
 		Comment res = new Comment();
+		User u = userService.findByPrincipal();
+		
+		Assert.notNull(rendezvous);
+		Assert.notNull(u);
+		
+		res.setRendezvous(rendezvous);
+		res.setReplies(new ArrayList<Comment>());
+		res.setCreationMoment(new Date());
+		res.setUser(u);
 
+		return res;
+	}
+	
+	public Comment createReply(Comment comment){
+		Comment res = new Comment();
+		User u = userService.findByPrincipal();
+		
+
+		Assert.notNull(comment);
+		Assert.notNull(u);
+		Assert.isNull(comment.getReplyingTo());
+		Assert.isTrue(rendezvousService.getRSVPRendezvousesForUser(u).contains(comment.getRendezvous()));
+		
+		res.setRendezvous(null);
+		res.setReplies(null);
+		res.setCreationMoment(new Date());
+		res.setReplyingTo(comment);
+		res.setUser(u);
+		
 		return res;
 	}
 
@@ -37,10 +76,16 @@ public class CommentService {
 	}
 
 	public Comment save(Comment comment) {
+		User u = userService.findByPrincipal();
+		Assert.notNull(comment);
+		Assert.notNull(u);
+		Assert.isTrue(rendezvousService.getRSVPRendezvousesForUser(u).contains(comment.getRendezvous()));
 		return commentRepository.save(comment);
 	}
 
-	public void delete(final Comment comment) {
+	public void deleteByAdmin(final Comment comment) {
+		Admin a = adminService.findByPrincipal();
+		Assert.notNull(a);
 		commentRepository.delete(comment);
 
 	}
