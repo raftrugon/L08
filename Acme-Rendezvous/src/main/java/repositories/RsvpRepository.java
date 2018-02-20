c44e04f8907751f94586ab6be3f53be3d5a53393
Armando6464 <armando95_95@hotmail.com>
2018-02-20 23:13:24 +0100

    Fixed questionsAndAnswers attribute name in UML Model

--- Acme-Rendezvous/src/main/java/repositories/RsvpRepository.java
+++ Acme-Rendezvous/src/main/java/repositories/RsvpRepository.java
@@ -1,6 +1,8 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,5 +11,8 @@ import domain.Rsvp;
 
 @Repository
 public interface RsvpRepository extends JpaRepository<Rsvp, Integer> {
+	
+	@Query("select r.rendezvous from Rsvp r where r.user = ?1")
+	Collection<String> getPendingQuestions(Rsvp rsvp);
 
 }
