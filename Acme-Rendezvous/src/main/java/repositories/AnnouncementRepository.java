57d4e19501147e38dfa3e7b362c31e00a2b19644
César García <cgpcsjcmp@gmail.com>
2018-02-26 16:49:01 +0100

    Fix: Queries de el listado de announcement
    -Ahora no muestran los announcements inapropiados.
    -Están comentadas las queries antiguas

--- Acme-Rendezvous/src/main/java/repositories/AnnouncementRepository.java
+++ Acme-Rendezvous/src/main/java/repositories/AnnouncementRepository.java
@@ -8,23 +8,24 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Announcement;
-import domain.Rendezvous;
 import domain.User;
 
 @Repository
 public interface AnnouncementRepository extends JpaRepository<Announcement, Integer> {
-	
-	@Query("select a from Announcement a order by creationMoment DESC")
+
+	@Query("select a from Announcement a where a.inappropriate = 0 order by creationMoment DESC")
 	Collection<Announcement> findAllOrdered();
 
-	@Query("select r.rendezvous.announcements from Rsvp r where r.user = ?1 order by creationMoment DESC")
+	//select r.rendezvous.announcements from Rsvp r where r.user = ?1 order by creationMoment DESC
+	@Query("select a from Rsvp r join r.rendezvous.announcements a where r.user = ?1 AND a.inappropriate = 0 order by creationMoment DESC")
 	Collection<Announcement> getRSVPAnnouncementsForUser(User user);
-	
-	@Query("select r.announcements from Rendezvous r where r.user = ?1 order by creationMoment DESC")
-	Collection<Announcement> getMyAnnouncements(User user);
-	
-	@Query("select r.announcements from Rendezvous r where r.id = ?1 order by creationMoment DESC")
-	Collection<Announcement> getRendezvousAnnouncementsSorted(int rendezvousId);
 
+	//select r.announcements from Rendezvous r where r.user = ?1 order by creationMoment DESC
+	@Query("select a from Rendezvous r join r.announcements a where r.user = ?1 AND a.inappropriate = 0 order by creationMoment DESC")
+	Collection<Announcement> getMyAnnouncements(User user);
+
+	//select r.announcements from Rendezvous r where r.id = ?1 order by creationMoment DESC
+	@Query("select a from Rendezvous r join r.announcements a where r.id = ?1 AND a.inappropriate = 0 order by creationMoment DESC")
+	Collection<Announcement> getRendezvousAnnouncementsSorted(int rendezvousId);
 
 }
